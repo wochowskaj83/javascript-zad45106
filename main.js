3ee100e9fbc2126c5ac07cac3a8ea171911212c3
wochowskaj83 <wochowskaj@gmail.com>
2023-04-13 18:45:45 +0200

    rozwiazanie zadania

---
+++ main.js
@@ -0,0 +1,11 @@
+function czysiezgadza(a, b) {
+    if (a + b > 20) {
+        return false;
+    } else if (a + b <= 20) {
+        return true;
+    } else {
+        return false;
+    }}
+
+
+    console.log(czysiezgadza(20, 0))
